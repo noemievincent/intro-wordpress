bd771c39b8922f8ebc0e3ae38e3069dcdf173a06
noemievincent <noemie.vincent1@student.hepl.be>
2022-02-23 14:28:05 +0100

    Added dw_trips_function

--- wp-content/themes/dw/functions.php
+++ wp-content/themes/dw/functions.php
@@ -19,3 +19,16 @@ register_post_type('trip', [
     'menu_icon' => 'dashicons-airplane',
     'supports' => ['title', 'editor', 'thumbnail'],
 ]);
+
+// Récupérer les trips via une requête Wordpress
+function dw_get_trips($count = 20) {
+    // 1. on instancie l'objet WP_Query
+    $trips = new WP_Query([
+        'post_type' => 'trip',
+        'orderby' => 'date',
+        'order' => 'DESC',
+        'posts_per_page' => $count,
+    ]);
+    // 2. on retourne l'objet WP_Query
+    return $trips;
+}
