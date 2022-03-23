88146f14d8e1f9ab0983016f3f1770bfb33f1f1b
noemievincent <noemie.vincent1@student.hepl.be>
2022-03-23 16:06:25 +0100

    Updated form validation

--- wp-content/themes/dw/functions.php
+++ wp-content/themes/dw/functions.php
@@ -3,6 +3,25 @@
 // Charger les fichiers nécessaires
 require_once(__DIR__ . '/Menus/PrimaryMenuWalker.php');
 require_once(__DIR__ . '/Menus/PrimaryMenuItem.php');
+require_once(__DIR__ . '/Forms/BaseFormController.php');
+require_once(__DIR__ . '/Forms/ContactFormController.php');
+require_once(__DIR__ . '/Forms/Sanitizers/BaseSanitizer.php');
+require_once(__DIR__ . '/Forms/Sanitizers/TextSanitizer.php');
+require_once(__DIR__ . '/Forms/Sanitizers/EmailSanitizer.php');
+require_once(__DIR__ . '/Forms/Validators/BaseValidator.php');
+require_once(__DIR__ . '/Forms/Validators/RequiredValidator.php');
+require_once(__DIR__ . '/Forms/Validators/EmailValidator.php');
+require_once(__DIR__ . '/Forms/Validators/AcceptedValidator.php');
+
+// Lancer la sessions PHP pour pouvoir passer des variables de page en page
+add_action('init', 'dw_start_session', 1);
+
+function dw_start_session()
+{
+    if (! session_id()) {
+        session_start();
+    }
+}
 
 // Désactiver l'éditeur "Gutenberg" de Wordpress
 add_filter('use_block_editor_for_post', '__return_false');
@@ -10,7 +29,7 @@ add_filter('use_block_editor_for_post', '__return_false');
 // Activer les images sur les articles
 add_theme_support('post-thumbnails');
 
-// Enregistrer un custom post-type pour "nos voyages"
+// Enregistrer un seul custom post-type pour "nos voyages"
 register_post_type('trip', [
     'label' => 'Voyages',
     'labels' => [
@@ -21,7 +40,7 @@ register_post_type('trip', [
     'public' => true,
     'menu_position' => 5,
     'menu_icon' => 'dashicons-airplane',
-    'supports' => ['title', 'editor', 'thumbnail'],
+    'supports' => ['title','editor','thumbnail'],
     'rewrite' => ['slug' => 'voyages'],
 ]);
 
@@ -32,14 +51,14 @@ register_post_type('message', [
         'name' => 'Messages de contact',
         'singular_name' => 'Message de contact',
     ],
-    'description' => 'Les messages envoyés par le formulaire de contact',
+    'description' => 'Les messages envoyés par le formulaire de contact.',
     'public' => false,
     'show_ui' => true,
     'menu_position' => 15,
     'menu_icon' => 'dashicons-buddicons-pm',
     'capabilities' => [
         'create_posts' => false,
-        'read_posts' => true,
+        'read_post' => true,
         'read_private_posts' => true,
         'edit_posts' => true,
     ],
@@ -56,45 +75,49 @@ function dw_get_trips($count = 20)
         'order' => 'DESC',
         'posts_per_page' => $count,
     ]);
+
     // 2. on retourne l'objet WP_Query
     return $trips;
 }
 
-// Enregister les zones de menus
+// Enregistrer les zones de menus
+
 register_nav_menu('primary', 'Navigation principale (haut de page)');
 register_nav_menu('footer', 'Navigation de pied de page');
 
-// Fonction pour récupérer les éléments d'un menu sous forme d'un tableau d'objet
+// Fonction pour récupérer les éléments d'un menu sous forme d'un tableau d'objets
+
 function dw_get_menu_items($location)
 {
     $items = [];
 
-    // Récupérer le menu WordPress pour $location
+    // Récupérer le menu Wordpress pour $location
     $locations = get_nav_menu_locations();
 
-    if (!($locations[$location] ?? false)) {
+    if(! ($locations[$location] ?? false)) {
         return $items;
     }
 
     $menu = $locations[$location];
+
     // Récupérer tous les éléments du menu récupéré
     $posts = wp_get_nav_menu_items($menu);
 
     // Formater chaque élément dans une instance de classe personnalisée
     // Boucler sur chaque $post
-    foreach ($posts as $post) {
+    foreach($posts as $post) {
         // Transformer le WP_Post en une instance de notre classe personnalisée
         $item = new PrimaryMenuItem($post);
 
-        // Ajouter au tableau d'éléments de niveau 0
-        if (!$item->isSubItem()) {
+        // Ajouter au tableau d'éléments de niveau 0.
+        if(! $item->isSubItem()) {
             $items[] = $item;
             continue;
         }
 
-        // Ajouter $item comme "enfant" de l'item parent
-        foreach ($items as $parent) {
-            if (!$parent->isParentFor($item)) continue;
+        // Ajouter $item comme "enfant" de l'item parent.
+        foreach($items as $parent) {
+            if(! $parent->isParentFor($item)) continue;
 
             $parent->addSubItem($item);
         }
@@ -110,73 +133,28 @@ add_action('admin_post_submit_contact_form', 'dw_handle_submit_contact_form');
 
 function dw_handle_submit_contact_form()
 {
-    $nonce = $_POST['_wpnonce'];
-
-    if (wp_verify_nonce($nonce, 'nonce_submit_contact')) {
-        // TODO : retourner un message d'erreur "not authorized"
-    }
-
-    $data = dw_sanitize_contact_form_data();
-
-    if ($errors = dw_validate_contact_form_data($data)) {
-        // Pas OK
-        return $errors;
-    }
-
-    // C'est OK
-
-    $id = wp_insert_post([
-        'post_title' => 'Message de ' . $data['firstname'] . ' ' . $data['lastname'],
-        'post_type' => 'message',
-        'post_content' => $data['message'],
-        'post_status' => 'publish',
-    ]);
-
-    // Générer un email contenant l'url vers le post en question
-    $feedback = 'Bonjour, vous avez un nouveau message.';
-    $feedback .= ' Y accéder : ' . get_edit_post_link($id);
-
-    // Envoyer l'email à l'admin
-    wp_mail(get_bloginfo('admin_email'), 'Nouveau message !', $feedback);
-
-    // TODO : retourner un message d'erreur "not authorized"
+    // Instancier le controlleur du formulaire
+    $form = new ContactFormController($_POST);
 }
 
-function dw_sanitize_contact_form_data()
+function dw_get_contact_field_value($field)
 {
-    return [
-        'firstname' => sanitize_text_field($_POST['firstname']),
-        'lastname' => sanitize_text_field($_POST['lastname']),
-        'email' => sanitize_email($_POST['email']),
-        'phone' => sanitize_text_field($_POST['phone']),
-        'message' => sanitize_text_field($_POST['message'] ?? ''),
-        'rules' => sanitize_text_field($_POST['rules'] ?? ''),
-    ];
-}
-
-function dw_validate_contact_form_data($data)
-{
-    $errors = [];
-    $required = ['firstname', 'lastname', 'email', 'message'];
-    $email = ['email'];
-    $accepted = ['rules'];
-
-    foreach ($data as $key => $value) {
-        if (in_array($key, $required) && !$value) {
-            $errors[$key] = 'required';
-            continue;
-        }
-
-        if (in_array($key, $email) && !filter_var($value, FILTER_VALIDATE_EMAIL)) {
-            $errors[$key] = 'email';
-            continue;
-        }
-
-        if (in_array($key, $accepted) && $value != '1') {
-            $errors[$key] = 'accepted';
-            continue;
-        }
+    if(! isset($_SESSION['contact_form_feedback'])) {
+        return '';
     }
 
-    return $errors ? false : true;
+    return $_SESSION['contact_form_feedback']['data'][$field] ?? '';
+}
+
+function dw_get_contact_field_error($field)
+{
+    if(! isset($_SESSION['contact_form_feedback'])) {
+        return '';
+    }
+
+    if(! ($_SESSION['contact_form_feedback']['errors'][$field] ?? null)) {
+        return '';
+    }
+
+    return '<p>' . $_SESSION['contact_form_feedback']['errors'][$field] . '</p>';
 }
--- wp-content/themes/dw/template-contact.php
+++ wp-content/themes/dw/template-contact.php
@@ -1,44 +1,57 @@
 <?php /* Template Name: Contact page template */ ?>
 <?php get_header(); ?>
-<?php if(have_posts()): while (have_posts()): the_post(); ?>
+<?php if(have_posts()): while(have_posts()): the_post(); ?>
     <main class="layout contact">
         <h2 class="contact__title"><?= get_the_title(); ?></h2>
         <div class="contact__content">
             <?php the_content(); ?>
         </div>
-        <form class="contact__form" action="<?= get_home_url(); ?>/wp-admin/admin-post.php" method="POST">
-            <div class="form__field">
-                <label for="firstname" class="form__label">Votre prénom</label>
-                <input type="text" name="firstname" id="firstname" class="form__input">
-            </div>
-            <div class="form__field">
-                <label for="lastname" class="form__label">Votre nom</label>
-                <input type="text" name="lastname" id="lastname" class="form__input">
-            </div>
-            <div class="form__field">
-                <label for="email" class="form__label">Votre adresse e-mail</label>
-                <input type="email" name="email" id="email" class="form__input">
-            </div>
-            <div class="form__field">
-                <label for="phone" class="form__label">Votre téléphone</label>
-                <input type="tel" name="phone" id="phone" class="form__input">
-            </div>
-            <div class="form__field">
-                <label for="message" class="form__label">Votre message</label>
-                <textarea name="message" id="message" cols="30" rows="10" class="form__input"></textarea>
-            </div>
-            <div class="form__field">
-                <label for="rules" class="form__label">
-                    <input type="checkbox" id="rules" value="1">
-                    <span class="form__checklabel">J'accepte les <a href="#">conditions générales d'utilisations</a>.</span>
-                </label>
-            </div>
-            <div class="form__actions">
-                <?php wp_nonce_field('nonce_submit_contact'); ?>
-                <input type="hidden" name="action" value="submit_contact_form"/>
-                <button class="form__button" type="submit">Envoyer</button>
-            </div>
-        </form>
+        <?php if(! isset($_SESSION['contact_form_feedback']) || ! $_SESSION['contact_form_feedback']['success']) : ?>
+            <form action="<?= get_home_url(); ?>/wp-admin/admin-post.php" method="POST" class="contact__form form" id="contact">
+                <?php if(isset($_SESSION['contact_form_feedback'])) : ?>
+                    <p>Oups ! Il y a des erreurs dans le formulaire</p>
+                <?php endif; ?>
+                <div class="form__field">
+                    <label for="firstname" class="form__label">Votre prénom</label>
+                    <input type="text" name="firstname" id="firstname" class="form__input" value="<?= dw_get_contact_field_value('firstname'); ?>">
+                    <?= dw_get_contact_field_error('firstname'); ?>
+                </div>
+                <div class="form__field">
+                    <label for="lastname" class="form__label">Votre nom</label>
+                    <input type="text" name="lastname" id="lastname" class="form__input" value="<?= dw_get_contact_field_value('lastname'); ?>">
+                    <?= dw_get_contact_field_error('lastname'); ?>
+                </div>
+                <div class="form__field">
+                    <label for="email" class="form__label">Votre adresse e-mail</label>
+                    <input type="email" name="email" id="email" class="form__input" value="<?= dw_get_contact_field_value('email'); ?>">
+                    <?= dw_get_contact_field_error('email'); ?>
+                </div>
+                <div class="form__field">
+                    <label for="phone" class="form__label">Votre numéro de téléphone</label>
+                    <input type="tel" name="phone" id="phone" class="form__input" value="<?= dw_get_contact_field_value('phone'); ?>">
+                    <?= dw_get_contact_field_error('phone'); ?>
+                </div>
+                <div class="form__field">
+                    <label for="message" class="form__label">Votre message</label>
+                    <textarea name="message" id="message" cols="30" rows="10" class="form__input"><?= dw_get_contact_field_value('message'); ?></textarea>
+                    <?= dw_get_contact_field_error('message'); ?>
+                </div>
+                <div class="form__field">
+                    <label for="rules" class="form__checkbox">
+                        <input type="checkbox" name="rules" id="rules" value="1" />
+                        <span class="form__checklabel">J'accepte les <a href="#">conditions générales d'utilisation</a>.</span>
+                    </label>
+                    <?= dw_get_contact_field_error('rules'); ?>
+                </div>
+                <div class="form__actions">
+                    <?php wp_nonce_field('nonce_submit_contact'); ?>
+                    <input type="hidden" name="action" value="submit_contact_form" />
+                    <button class="form__button" type="submit">Envoyer</button>
+                </div>
+            </form>
+        <?php else : ?>
+            <p id="contact">Merci ! Votre message a bien été envoyé.</p>
+            <?php unset($_SESSION['contact_form_feedback']); endif; ?>
     </main>
 <?php endwhile; endif; ?>
 <?php get_footer(); ?>
